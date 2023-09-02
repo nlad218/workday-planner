07d77bb3b75f6bf93a7db4a71b4f82ecaaf2adb7
Nick Deignan <nickdeignan@Nicks-MacBook-Pro.local>
2023-09-02 12:11:51 -0600

    added hour blocks in html and created javascript code to display current day and label hour blocks according to current time

--- script.js
+++ script.js
@@ -15,9 +15,22 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  var currentHour = dayjs().hour();
+
+  for (var i = 9; i < 18; i++) {
+    if (i < currentHour) {
+      $("#hour-" + i).addClass("past");
+    } else if (i === currentHour) {
+      $("#hour-" + i).addClass("present");
+    } else {
+      $("#hour-" + i).addClass("future");
+    }
+  }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format("dddd, MMM, D");
+  $("#currentDay").text(currentDate);
 });
